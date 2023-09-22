f558e424760fa4d48d90954f9ca9fb86750bae55
PIAL <PIAL@JASTER>
2023-09-23 00:18:09 +0600

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Task1.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+
+
+        public ActionResult Home1()
+        {
+            return View();
+        }
+
+
+        public ActionResult Education()
+        {
+            return View();
+        }
+
+
+        public ActionResult PersonalInfo()
+        {
+            return View();
+        }
+
+
+
+        public ActionResult Projects()
+        {
+            return View();
+        }
+
+
+        public ActionResult References()
+        {
+            return View();
+        }
+    }
+
+
+
+}
