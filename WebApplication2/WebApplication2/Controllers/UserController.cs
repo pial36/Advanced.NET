f4319155293c4ca1fa8fae149a14c5d812a5ec44
pial36 <pialhassanchowdhury@gmail.com>
2023-10-08 22:11:16 +0600

    First Commit

---
+++ WebApplication2/WebApplication2/Controllers/UserController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication2.Controllers
+{
+    public class UserController : Controller
+    {
+        
+        public ActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Register(string name, string userId, string password, string id, string email, DateTime dob)
+        {
+     
+            return RedirectToAction("RegistrationSuccess");
+        }
+
+        public ActionResult RegistrationSuccess()
+        {
+            return View();
+        }
+    }
+}
